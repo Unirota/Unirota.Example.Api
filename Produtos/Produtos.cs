c1c11b1543e465cb0b522552b0a9033ef0c4a0a9
José Ricardo <josericardomgomes@gmail.com>
2024-08-05 00:26:57 -0300

    crudProduto
    Lista TO DO feita e crud de Produtos criado

---
+++ Produtos/Produtos.cs
@@ -0,0 +1,24 @@
+namespace Unirota.Example.Api.Produtos;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string Nome { get; private set; } // setter privado, para que ninguém fora da classe consiga alterá-lo
+    public decimal Preco { get; private set; }
+
+    public Produto(string nome, decimal preco)
+    {
+        Nome = nome;
+        Preco = preco;
+    }
+
+    public void AtualizarNome(string nome)
+    {
+        Nome = nome;
+    }
+
+    public void AtualizarPreco(decimal preco)
+    {
+        Preco = preco;
+    }
+}
